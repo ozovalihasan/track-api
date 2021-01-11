b8cef74ce55cbb4c2d92e6134ca7fd66bcfc0e94
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-11 11:25:56 +0300

    Add as_json method for taken_time model

--- app/models/taken_time.rb
+++ app/models/taken_time.rb
@@ -1,3 +1,16 @@
 class TakenTime < ApplicationRecord
   belongs_to :piece
+
+  def as_json(options={})
+
+    {
+        :id => self.id,
+        :created_at => self.created_at.to_time.to_i,
+        :updated_at => self.updated_at.to_time.to_i,
+        :piece => {id: self.piece.id, name: self.piece.name},
+        :tracked_item_id => self.piece.tracked_item.id
+    }
+
+  end
+
 end

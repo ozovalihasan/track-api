b8ce79243e8e8ccb46e9b6f719c8ec740565ba46
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-11 11:25:20 +0300

    Update taken_times route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resource :users, only: [:create]
   resources :tracked_items
   resources :pieces
-  resources :taken_times
+  resources :taken_times , only: [:index, :create, :destroy]
   
   post "/login", to: "users#login"
   get "auto_login", to: "users#auto_login"

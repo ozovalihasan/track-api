b5c9607e959205ea852456102baa88582b5285bd
Hasan Ozovali <ozovalihasan@gmail.com>
2021-01-11 11:22:26 +0300

    Add relations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: { case_sensitive: true }, presence: true
 
   has_many :tracked_items, dependent: :destroy
+  has_many :pieces, through: :tracked_items
+  has_many :taken_times, through: :pieces
 end
